b788b41856c5029842e2b0adc72d0d8c2244af87
CLSigma <korzenikn@gmail.com>
2022-12-09 14:51:34 +0100

    Check logged in user

--- force-app/main/default/lwc/mainPage/mainPage.js
+++ force-app/main/default/lwc/mainPage/mainPage.js
@@ -1,12 +1,19 @@
 import { LightningElement } from 'lwc';
+import Id from '@salesforce/user/Id';
 
 import icons from '@salesforce/resourceUrl/otherImages';
 
 export default class MainPage extends LightningElement {
+
+    Id = Id;
 
     previous = icons + '/otherImages/previous.png';
     next = icons + '/otherImages/next.png';
 
     loggedInUser = true;
     username = 'user13';
+
+    connectedCallback(){
+        console.log(this.Id);
+    }
 }
