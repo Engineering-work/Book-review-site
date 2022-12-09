673344c409c64b65e4102b4060439e5d02d5260f
CLSigma <korzenikn@gmail.com>
2022-12-09 14:50:40 +0100

    Settings intial stylig

--- force-app/main/default/lwc/popUp/popUp.js
+++ force-app/main/default/lwc/popUp/popUp.js
@@ -3,8 +3,11 @@ import { LightningElement, api } from 'lwc';
 export default class PopUp extends LightningElement {
     @api popuptype;
     @api ispopupactive;
-    addRating= false;
-    addRating= true;
+    addRating = false;
+    addBook = false;
+    editUserData = false;
+    username = 'user13';
+    name = 'Mariia';
     
     closeModal(){
         this.ispopupactive = false;
@@ -19,8 +22,23 @@ export default class PopUp extends LightningElement {
         if(this.popuptype === 'addRating'){
             this.addRating = true;
         }
-        else if(this.popuptype === 'addBook'){
+        else if(this.popuptype === 'addBookToMyList'){
             this.addBook = true;
         }
+        else if(this.popuptype === 'editUserData'){
+            this.editUserData = true;
+        }
+    }
+
+    changeDataHandler(event){
+        let clicked = event.target.id.split('-')[0]
+        let field = this.template.querySelector(`lightning-input[id^="${clicked}"]`);
+        if(clicked==='username'){
+            
+        }
+        else{
+            
+        }
+
     }
 }
