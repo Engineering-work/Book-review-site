9661554c4115c821ba4062b0292f112d66987397
CLSigma <korzenikn@gmail.com>
2022-12-11 18:08:39 +0100

    Add book to my list styling

--- force-app/main/default/lwc/popUp/popUp.js
+++ force-app/main/default/lwc/popUp/popUp.js
@@ -1,13 +1,33 @@
-import { LightningElement, api } from 'lwc';
-
+import { LightningElement, api, wire, track } from 'lwc';
+import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi';
+import BookListObject from "@salesforce/schema/Book_List_Item__c";
+import BookStatus from "@salesforce/schema/Book_List_Item__c.Status__c";
 export default class PopUp extends LightningElement {
     @api popuptype;
     @api ispopupactive;
+    @track titleValue;
+    @track selectedTitle = false;
     addRating = false;
-    addBook = false;
+    addBookToMyList = false;
     editUserData = false;
     username = 'user13';
     name = 'Mariia';
+    bookId;
+
+
+    @wire(getObjectInfo, {objectApiName: BookListObject}) bookListInfo;
+
+    @wire(getPicklistValues, {recordTypeId: '$bookListInfo.data.defaultRecordTypeId', fieldApiName: BookStatus}) status;
+
+    onTitleSelection(event){
+        this.titleValue = event.detail.selectedTitle;
+        console.log(this.titleValue);
+        if(this.titleValue!==null){
+            this.selectedTitle = true;
+        }
+        this.bookId = event.detail.selectedBookId;
+        console.log(this.bookId);
+    }
     
     closeModal(){
         this.ispopupactive = false;
@@ -23,7 +43,7 @@ export default class PopUp extends LightningElement {
             this.addRating = true;
         }
         else if(this.popuptype === 'addBookToMyList'){
-            this.addBook = true;
+            this.addBookToMyList = true;
         }
         else if(this.popuptype === 'editUserData'){
             this.editUserData = true;
